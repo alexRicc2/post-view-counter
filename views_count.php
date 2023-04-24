3c648704d5452458b7db4819f6035682cd87be4e
alexRicc2 <alex.65.santanna@gmail.com>
2023-04-23 22:39:22 -0300

    set post views

--- views_count.php
+++ views_count.php
@@ -45,3 +45,23 @@ add_action('rest_api_init', function () {
     'callback' => 'my_increment_views_count',
   ));
 });
+
+function set_views_count($request)
+{
+  $post_slug = $request->get_param('slug');
+  $post = get_page_by_path('/' . $post_slug, OBJECT, 'post');
+  $post_id = $post ? $post->ID : null;
+  $views_count = $request->get_param('count');
+  if (empty($views_count)) {
+    return new WP_Error('empty_count_param', 'Count parameter is required', array('status' => 400));
+  }
+  update_post_meta($post_id, 'views_count', $views_count);
+  return $views_count;
+}
+
+add_action('rest_api_init', function () {
+  register_rest_route('set-views-count/v1', '/(?P<slug>[a-z0-9-]+)', array(
+    'methods' => 'POST',
+    'callback' => 'set_views_count',
+  ));
+});
